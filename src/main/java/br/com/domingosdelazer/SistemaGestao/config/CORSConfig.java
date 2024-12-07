f8708dcbb6d0e19836aa4b6c9765f8f8dbfab948
Victor Oliveira <93015739+VicThor99@users.noreply.github.com>
2024-12-07 15:59:06 -0300

    cors config

---
+++ src/main/java/br/com/domingosdelazer/SistemaGestao/config/CORSConfig.java
@@ -0,0 +1,24 @@
+package br.com.domingosdelazer.SistemaGestao.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://domingodelazer.click", "http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
