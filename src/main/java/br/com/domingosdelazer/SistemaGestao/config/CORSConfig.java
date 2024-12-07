7fd60ffa67c0c730f1ba8787cc89c644a55d2117
Victor Oliveira <93015739+VicThor99@users.noreply.github.com>
2024-12-07 17:21:48 -0300

    Update CORSConfig.java

--- src/main/java/br/com/domingosdelazer/SistemaGestao/config/CORSConfig.java
+++ src/main/java/br/com/domingosdelazer/SistemaGestao/config/CORSConfig.java
@@ -1,24 +1,19 @@
 package br.com.domingosdelazer.SistemaGestao.config;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CORSConfig {
+public class CORSConfig implements WebMvcConfigurer{
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://domingodelazer.click", "http://localhost:8080")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .maxAge(3600);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
