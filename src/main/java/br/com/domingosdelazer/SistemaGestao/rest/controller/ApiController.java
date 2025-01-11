cf1c22e28bde575073c72cbb1c9b68ddc9fbb127
Victor Oliveira <93015739+VicThor99@users.noreply.github.com>
2025-01-10 22:28:03 -0300

    Ajustando configurações

--- src/main/java/br/com/domingosdelazer/SistemaGestao/rest/controller/ApiController.java
+++ src/main/java/br/com/domingosdelazer/SistemaGestao/rest/controller/ApiController.java
@@ -1,12 +1,14 @@
 package br.com.domingosdelazer.SistemaGestao.rest.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping
+@CrossOrigin(origins = "https://www.domingodelazer.click", allowedHeaders = "*", allowCredentials = "true", methods = {RequestMethod.OPTIONS, RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
 public class ApiController {
 
     @RequestMapping(value = "/**", method = RequestMethod.OPTIONS)
