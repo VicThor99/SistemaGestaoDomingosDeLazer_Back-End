07d4a17122bc19060c4009ba0c490b72230e2a97
Victor Oliveira <93015739+VicThor99@users.noreply.github.com>
2025-03-11 09:42:35 -0300

    Corrigindo save Serie

--- src/main/java/br/com/domingosdelazer/SistemaGestao/repository/SalaRepository.java
+++ src/main/java/br/com/domingosdelazer/SistemaGestao/repository/SalaRepository.java
@@ -12,6 +12,6 @@ public interface SalaRepository extends JpaRepository<Sala, Integer> {
     @Query(nativeQuery = true, value = "select distinct(sa.sala) from serie s inner join sala sa on sa.id = s.sala_id where s.escola_id = :escolaId")
     List<String> listSalasString(@Param("escolaId") Integer escolaId);
 
-    @Query(nativeQuery = true, value = "select s.sala from sala s where s.sala = :nomeSala")
+    @Query(nativeQuery = true, value = "select * from sala s where s.sala = :nomeSala")
     Sala getSalaPorNomeSala(@Param("nomeSala") String nomeSala);
 }
